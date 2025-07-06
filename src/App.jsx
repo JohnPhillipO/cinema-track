280d0fe323a06ecc9c73f76ffa7bbc574b193cca
John Phillip Ostiano (J.P.) <jostiano@gmail.com>
2025-07-06 05:02:49 -0400

    add movie and timer page plus more navbar styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LayoutRoute from "./layout/LayoutRoute";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Timer from "./pages/Timer";
+import Movies from "./pages/Movies";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<LayoutRoute />}>
           <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/timer" element={<Timer />} />
+          <Route path="/movies" element={<Movies />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,54 @@
-import { NavLink } from "react-router-dom";
-import { PiFilmSlate } from "react-icons/pi";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
   return (
-    <header className="font-inter flex justify-between items-center px-6 py-6">
-      <div className="text-3xl flex items-center gap-2">
-        <PiFilmSlate size={40} />
+    <header className="font-inter flex justify-between items-center px-6 py-6 sm:px-15 sm:py-6">
+      <div
+        className="text-3xl flex items-center gap-2 cursor-pointer"
+        onClick={() => navigate("/")}
+      >
         <h1 className="font-semibold">Cinetracker</h1>
       </div>
-      <nav className="flex items-center gap-5">
+      <nav className="flex items-center gap-10">
         <NavLink
           to="/about"
-          className={(isActive) => (isActive ? "" : "underline")}
+          className={({ isActive }) =>
+            `rounded-3xl transition-all duration-200 ${
+              isActive
+                ? "bg-gray-50 border-solid border-2 border-gray-300 px-3 py-1 "
+                : "hover:bg-gray-50 hover:border-solid hover:border-2 hover:border-gray-300 hover:px-3 hover:py-1"
+            }`
+          }
         >
           About
         </NavLink>
-        <NavLink to="/timer">Timer</NavLink>
-        <NavLink to="/movies">Movies</NavLink>
+        <NavLink
+          to="/timer"
+          className={({ isActive }) =>
+            `rounded-3xl transition-all duration-200 ${
+              isActive
+                ? "bg-gray-50 border-solid border-2 border-gray-300 px-3 py-1 "
+                : "hover:bg-gray-50 hover:border-solid hover:border-2 hover:border-gray-300 hover:px-3 hover:py-1"
+            }`
+          }
+        >
+          Timer
+        </NavLink>
+        <NavLink
+          to="/movies"
+          className={({ isActive }) =>
+            `rounded-3xl transition-all duration-200 ${
+              isActive
+                ? "bg-gray-50 border-solid border-2 border-gray-300 px-3 py-1 "
+                : "hover:bg-gray-50 hover:border-solid hover:border-2 hover:border-gray-300 hover:px-3 hover:py-1 "
+            }`
+          }
+        >
+          Movies
+        </NavLink>
       </nav>
-      <button className="bg-blue-400 px-6 py-2 rounded-3xl text-white">
+      <button className="bg-blue-400 px-6 py-2 rounded-3xl text-white font-semibold">
         Sign in
       </button>
     </header>
