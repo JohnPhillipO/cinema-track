840bbd0e5d02aafc1b18004a65520f9d2fc0004f
John Phillip Ostiano (J.P.) <jostiano@gmail.com>
2025-07-06 03:57:43 -0400

    add react router dom routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LayoutRoute from "./layout/LayoutRoute";
+import Home from "./pages/Home";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LayoutRoute />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
