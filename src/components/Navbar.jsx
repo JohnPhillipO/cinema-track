90d205b5b7d226f11c33eaf5b4a693ff3b09ec4e
John Phillip Ostiano (J.P.) <jostiano@gmail.com>
2025-07-06 09:13:49 -0400

    fix motion and styling for home and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,22 @@
 import { NavLink, useNavigate } from "react-router-dom";
+import { RxLapTimer } from "react-icons/rx";
 
 export default function Navbar() {
   const navigate = useNavigate();
   return (
     <header className="font-inter flex justify-between items-center px-6 py-6 fixed top-0 left-0 right-0 z-10 bg-white">
       <div
-        className="text-3xl flex items-center gap-2 cursor-pointer"
+        className="text-3xl flex items-center justify-center gap-2 cursor-pointer text-gray-900"
         onClick={() => navigate("/")}
       >
+        <RxLapTimer className="text-4xl" />
         <h1 className="font-semibold">Cinetracker</h1>
       </div>
       <nav className="hidden sm:flex sm:items-center sm:justify-center sm:gap-4">
         <NavLink
           to="/about"
           className={({ isActive }) =>
-            `rounded-3xl transition-all duration-200 px-3 py-1 w-20 text-center ${
+            `rounded-3xl transition-all duration-200 px-3 py-1 w-20 text-center text-gray-900 ${
               isActive
                 ? "bg-gray-50 border-solid border-2 border-gray-300"
                 : "hover:border-solid hover:border-2 hover:border-gray-300"
@@ -26,7 +28,7 @@ export default function Navbar() {
         <NavLink
           to="/timer"
           className={({ isActive }) =>
-            `rounded-3xl transition-all duration-200 px-3 py-1 w-20 text-center ${
+            `rounded-3xl transition-all duration-200 px-3 py-1 w-20 text-center text-gray-900 ${
               isActive
                 ? "bg-gray-50 border-solid border-2 border-gray-300 "
                 : "hover:border-solid hover:border-2 hover:border-gray-300"
@@ -38,7 +40,7 @@ export default function Navbar() {
         <NavLink
           to="/movies"
           className={({ isActive }) =>
-            `rounded-3xl transition-all duration-200 px-3 py-1 w-20 text-center ${
+            `rounded-3xl transition-all duration-200 px-3 py-1 w-20 text-center text-gray-900 ${
               isActive
                 ? "bg-gray-50 border-solid border-2 border-gray-300"
                 : "hover:border-solid hover:border-2 hover:border-gray-300"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,41 @@
+import { motion } from "motion/react";
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
+  const transition = {
+    duration: 1,
+    delay: 0.2,
+    ease: [0, 0.71, 0.2, 1.01],
+  };
+
+  const navigate = useNavigate();
+
   return (
     <>
       <section className="flex flex-col items-center justify-center h-screen text-center font-inter bg-gray-50">
-        <h1 className="text-7xl font-bold font-inter">
-          <span className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-600 to-blue-300">
-            Earn your screen time.
-          </span>
-        </h1>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={transition}
+        >
+          <h1 className="text-7xl font-bold font-inter">
+            <span className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-600 to-blue-300">
+              Earn your screen time.
+            </span>
+          </h1>
+        </motion.div>
         <p className="text-gray-950 mt-4 max-w-2xl font-inter text-lg">
           Work smart. Watch guilt-free. Track your progress and enjoy screen
           time - when you’ve earned it.
         </p>
-        <button className="bg-blue-500 text-white px-6 py-3 rounded-3xl mt-15 cursor-pointer font-inter text-lg hover:bg-blue-600 transition-all duration-200">
+        <button
+          className="bg-blue-500 text-white px-6 py-3 rounded-3xl mt-15 cursor-pointer font-inter text-lg hover:bg-blue-600 transition-all duration-200"
+          onClick={() => navigate("/timer")}
+        >
           Get Started
         </button>
       </section>
+      {/* Features */}
     </>
   );
 }
